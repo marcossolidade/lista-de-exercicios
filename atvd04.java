8a225ff9930146acbcf48ee2bdebab9ef30c3d19
marcossolidade <70726335+marcossolidade@users.noreply.github.com>
2022-06-08 11:46:44 -0300

    Add files via upload

---
+++ atvd04.java
@@ -0,0 +1,20 @@
+package atvd04;
+
+public class atvd04{
+
+    public static void main(String[] args) {
+       Integer soma=0;
+       Long mult=(long) 1;
+
+       for(int i=1;i<30;i++){
+           if((i%2)==1){
+               soma += i;
+           }else{
+               mult *= i;
+           }
+       }
+       System.out.print("O valor da soma é de:"+soma+"\n");
+       System.out.print("O valor da multiplicação é de: "+mult+"\n");
+    }
+}
+
