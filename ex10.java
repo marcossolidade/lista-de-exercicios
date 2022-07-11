1fcff350156e3c116d6fdd2aad5623aa9f77c994
Marcos Cauã <70726335+marcossolidade@users.noreply.github.com>
2022-07-11 11:00:50 -0300

    Add files via upload

---
+++ ex10.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import javax.swing.*;
+public class ex10 {
+    public static void main (String[] args){
+        float peso;
+        float altura;
+        float imc;
+        Scanner scan = new Scanner(System.in);
+
+
+                System.out.printf("Digite seu peso: ");
+                peso = scan.nextFloat();
+                System.out.printf("Digite seu altura: ");
+                altura = scan.nextFloat();
+                imc= peso/(altura*altura);
+                        if((imc >=18.5)&&(imc<=24.4 ));
+                JOptionPane.showMessageDialog( null, "Seu IMC é de:"+imc+"e sua situação é peso ideal ");
+                if((imc >=18.5)&&(imc<=24.4 ));
+                JOptionPane.showMessageDialog( null, "Seu IMC é de:"+imc+"e sua situação é peso ideal ");
+                if((imc >=24.5.5)&&(imc<=24.4 ));
+                JOptionPane.showMessageDialog( null, "Seu IMC é de:"+imc+"e sua situação é peso ideal ");
+                if((imc >=18.5)&&(imc<=24.4 ));
+                JOptionPane.showMessageDialog( null, "Seu IMC é de:"+imc+"e sua situação é peso ideal ");
+                if((imc >=18.5)&&(imc<=24.4 ));
+                JOptionPane.showMessageDialog( null, "Seu IMC é de:"+imc+"e sua situação é peso ideal ");
+
+    }
+}
+
